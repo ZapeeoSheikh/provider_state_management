b51b7ac84912adcc21e6b5738ed2cfd2eb8bc1af
ZapeeoSheikh <muhammadrmz871@gmail.com>
2024-03-01 17:59:05 +0500

    lec2 commit

--- lib/helper/widget/appbar_widget.dart
+++ lib/helper/widget/appbar_widget.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
-  const AppBarWidget({super.key});
+  const AppBarWidget({super.key, required this.opacityValue});
+
+  final double opacityValue;
+
   Size get preferredSize => const Size.fromHeight(60);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      backgroundColor: Colors.teal,
+      backgroundColor: opacityValue > 0.5
+          ? Colors.teal.withOpacity(opacityValue)
+          : Colors.pinkAccent.withOpacity(1 - opacityValue),
+      leading: opacityValue > 0.5
+          ? const Icon(Icons.sunny, color: Colors.white,)
+          : const Icon(Icons.nightlight_round, color: Colors.white,),
       title: const Text(
         "Provider State Management",
-        style: TextStyle(
-            fontSize: 20,
-            color: Colors.white
-        ),
+        style: TextStyle(fontSize: 20, color: Colors.white),
       ),
       actions: const [
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 8.0),
-          child: Text("#1", style: TextStyle(
-            color: Colors.white70, fontSize: 18
-          ),),
+          child: Text(
+            "#2",
+            style: TextStyle(color: Colors.white70, fontSize: 18),
+          ),
         )
       ],
     );
